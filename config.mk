aac7f7a1544770524c43bb7cf1bfc8016b5d93ec
tuckerrrrrrrrrr <tucker1014@hotmail.com>
2020-06-13 19:43:18 -0700

    fix build on openbsd

--- config.mk
+++ config.mk
@@ -7,6 +7,8 @@ VERSION = 1
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
+PKG_CONFIG = pkg-config
+
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
@@ -14,7 +16,8 @@ X11LIB = /usr/X11R6/lib
 INCS = -I. -I/usr/include -I/usr/include/freetype2 -I${X11INC}
 LIBS = -L/usr/lib -lc -lm -L${X11LIB} -lXft -lfontconfig -lX11 -lcairo
 # OpenBSD (uncomment)
-#INCS = -I. -I${X11INC} -I${X11INC}/freetype2
+INCS = -I. -I${X11INC} -I${X11INC}/freetype2 `${PKG_CONFIG} --cflags cairo`
+LIBS += -L/usr/local/lib
 # FreeBSD (uncomment)
 #INCS = -I. -I/usr/local/include -I/usr/local/include/freetype2 -I${X11INC}
 #LIBS = -L/usr/local/lib -lc -lm -L${X11LIB} -lXft -lfontconfig -lX11
